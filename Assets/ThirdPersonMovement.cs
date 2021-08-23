298e50a707ddfa474856dadcd051e0306cc500ab
Quinten97 <89324378+Quinten97@users.noreply.github.com>
2021-08-22 20:33:09 -0400

    No errors lol

--- Assets/ThirdPersonMovement.cs
+++ Assets/ThirdPersonMovement.cs
@@ -12,7 +12,7 @@ public class ThirdPersonMovement : MonoBehaviour
     void Update()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
-        float veritcal = Input.GetAxisRaw("Verticle");
+        float verticle = Input.GetAxisRaw("Verticle");
         Vector3 direction = new Vector3(horizontal, 0f, verticle).normalized;
 
         if (direction.magnitude >= 0.1f)
